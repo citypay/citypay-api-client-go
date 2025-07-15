f4786e403c0279fd17d9983703ec8abf17fdf86d
sjauncey-cp <167993631+sjauncey-cp@users.noreply.github.com>
2025-07-15 16:15:16 +0100

    Add unit tests for additional models

---
+++ test/model/model_direct_token_auth_request_test.go
@@ -0,0 +1,107 @@
+package citypay
+
+import (
+	"encoding/json"
+	openapiclient "github.com/citypay/citypay-api-client-go"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+var exampleNonce = "ABC"
+var exampleRedirectFail = "https://fail"
+var exampleRedirectSuccess = "https://ok"
+var exampleToken = "tok123"
+
+func buildExampleDirectTokenAuthRequest() *openapiclient.DirectTokenAuthRequest {
+	d := openapiclient.NewDirectTokenAuthRequest()
+	d.SetNonce(exampleNonce)
+	d.SetRedirectFailure(exampleRedirectFail)
+	d.SetRedirectSuccess(exampleRedirectSuccess)
+	d.SetToken(exampleToken)
+	return d
+}
+
+func TestNewDirectTokenAuthRequest(t *testing.T) {
+	model := openapiclient.NewDirectTokenAuthRequest()
+	require.NotNil(t, model)
+	assert.False(t, model.HasToken())
+}
+
+func TestNewDirectTokenAuthRequestWithDefaults(t *testing.T) {
+	model := openapiclient.NewDirectTokenAuthRequestWithDefaults()
+	require.NotNil(t, model)
+	assert.False(t, model.HasToken())
+}
+
+func TestDirectTokenAuthRequestSetGetCycle(t *testing.T) {
+	model := openapiclient.NewDirectTokenAuthRequest()
+	model.SetNonce(exampleNonce)
+	assert.True(t, model.HasNonce())
+	assert.Equal(t, exampleNonce, model.GetNonce())
+
+	model.SetRedirectFailure(exampleRedirectFail)
+	assert.True(t, model.HasRedirectFailure())
+	assert.Equal(t, exampleRedirectFail, model.GetRedirectFailure())
+
+	model.SetRedirectSuccess(exampleRedirectSuccess)
+	assert.True(t, model.HasRedirectSuccess())
+	assert.Equal(t, exampleRedirectSuccess, model.GetRedirectSuccess())
+
+	model.SetToken(exampleToken)
+	assert.True(t, model.HasToken())
+	assert.Equal(t, exampleToken, model.GetToken())
+}
+
+func TestDirectTokenAuthRequestJSONRoundTrip(t *testing.T) {
+	model := buildExampleDirectTokenAuthRequest()
+	data, err := json.Marshal(model)
+	require.NoError(t, err)
+
+	var unmarshalled openapiclient.DirectTokenAuthRequest
+	err = json.Unmarshal(data, &unmarshalled)
+	require.NoError(t, err)
+	assert.True(t, unmarshalled.HasToken())
+	assert.Equal(t, exampleToken, unmarshalled.GetToken())
+}
+
+func TestDirectTokenAuthRequestToMap(t *testing.T) {
+	model := buildExampleDirectTokenAuthRequest()
+	m, err := model.ToMap()
+	require.NoError(t, err)
+	if assert.Contains(t, m, "token") {
+		assert.Equal(t, &exampleToken, m["token"])
+	}
+}
+
+func TestNullableDirectTokenAuthRequestGetSet(t *testing.T) {
+	base := buildExampleDirectTokenAuthRequest()
+	n := openapiclient.NullableDirectTokenAuthRequest{}
+	n.Set(base)
+	require.True(t, n.IsSet())
+	assert.Equal(t, base, n.Get())
+}
+
+func TestNullableDirectTokenAuthRequestUnset(t *testing.T) {
+	base := buildExampleDirectTokenAuthRequest()
+	n := openapiclient.NewNullableDirectTokenAuthRequest(base)
+	require.True(t, n.IsSet())
+	n.Unset()
+	assert.False(t, n.IsSet())
+	assert.Nil(t, n.Get())
+}
+
+func TestNullableDirectTokenAuthRequestJSONRoundTrip(t *testing.T) {
+	base := buildExampleDirectTokenAuthRequest()
+	n := openapiclient.NewNullableDirectTokenAuthRequest(base)
+	data, err := json.Marshal(n)
+	require.NoError(t, err)
+
+	var newN openapiclient.NullableDirectTokenAuthRequest
+	err = json.Unmarshal(data, &newN)
+	require.NoError(t, err)
+	assert.True(t, newN.IsSet())
+	if assert.NotNil(t, newN.Get()) {
+		assert.Equal(t, exampleToken, newN.Get().GetToken())
+	}
+}
---
+++ test/model/model_domain_key_check_request_test.go
@@ -0,0 +1,88 @@
+package citypay
+
+import (
+	"encoding/json"
+	openapiclient "github.com/citypay/citypay-api-client-go"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+var exampleDomainKey = "ABCDEF"
+
+func buildExampleDomainKeyCheckRequest() *openapiclient.DomainKeyCheckRequest {
+	return openapiclient.NewDomainKeyCheckRequest(exampleDomainKey)
+}
+
+func TestNewDomainKeyCheckRequest(t *testing.T) {
+	model := openapiclient.NewDomainKeyCheckRequest(exampleDomainKey)
+	require.NotNil(t, model)
+	assert.Equal(t, exampleDomainKey, model.GetDomainKey())
+}
+
+func TestNewDomainKeyCheckRequestWithDefaults(t *testing.T) {
+	model := openapiclient.NewDomainKeyCheckRequestWithDefaults()
+	require.NotNil(t, model)
+	assert.Equal(t, "", model.GetDomainKey())
+}
+
+func TestDomainKeyCheckRequestSetGetCycle(t *testing.T) {
+	model := openapiclient.NewDomainKeyCheckRequest(exampleDomainKey)
+	model.SetDomainKey("XYZ")
+	assert.Equal(t, "XYZ", model.GetDomainKey())
+	if val, ok := model.GetDomainKeyOk(); assert.True(t, ok) {
+		assert.Equal(t, "XYZ", *val)
+	}
+}
+
+func TestDomainKeyCheckRequestJSONRoundTrip(t *testing.T) {
+	model := buildExampleDomainKeyCheckRequest()
+	data, err := json.Marshal(model)
+	require.NoError(t, err)
+
+	var unmarshalled openapiclient.DomainKeyCheckRequest
+	err = json.Unmarshal(data, &unmarshalled)
+	require.NoError(t, err)
+	assert.Equal(t, exampleDomainKey, unmarshalled.GetDomainKey())
+}
+
+func TestDomainKeyCheckRequestToMap(t *testing.T) {
+	model := buildExampleDomainKeyCheckRequest()
+	m, err := model.ToMap()
+	require.NoError(t, err)
+	if assert.Contains(t, m, "domain_key") {
+		assert.Equal(t, exampleDomainKey, m["domain_key"])
+	}
+}
+
+func TestNullableDomainKeyCheckRequestGetSet(t *testing.T) {
+	base := buildExampleDomainKeyCheckRequest()
+	n := openapiclient.NullableDomainKeyCheckRequest{}
+	n.Set(base)
+	require.True(t, n.IsSet())
+	assert.Equal(t, base, n.Get())
+}
+
+func TestNullableDomainKeyCheckRequestUnset(t *testing.T) {
+	base := buildExampleDomainKeyCheckRequest()
+	n := openapiclient.NewNullableDomainKeyCheckRequest(base)
+	require.True(t, n.IsSet())
+	n.Unset()
+	assert.False(t, n.IsSet())
+	assert.Nil(t, n.Get())
+}
+
+func TestNullableDomainKeyCheckRequestJSONRoundTrip(t *testing.T) {
+	base := buildExampleDomainKeyCheckRequest()
+	n := openapiclient.NewNullableDomainKeyCheckRequest(base)
+	data, err := json.Marshal(n)
+	require.NoError(t, err)
+
+	var newN openapiclient.NullableDomainKeyCheckRequest
+	err = json.Unmarshal(data, &newN)
+	require.NoError(t, err)
+	assert.True(t, newN.IsSet())
+	if assert.NotNil(t, newN.Get()) {
+		assert.Equal(t, exampleDomainKey, newN.Get().GetDomainKey())
+	}
+}
